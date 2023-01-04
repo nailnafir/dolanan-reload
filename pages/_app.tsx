b33fb3a95519cff9c2d1d5088113d2b47bf4d78a
Nailul Firdaus <nail.nafir@gmail.com>
2023-01-04 20:54:09 +0700

    convert plain HTML detail page to NextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/utilities.css'
 import '../styles/homepage.css'
+import '../styles/detail.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
