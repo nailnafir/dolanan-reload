e8f3917fc7612a2443858f72ae687edf35e341e2
Nailul Firdaus <nail.nafir@gmail.com>
2023-01-10 18:45:27 +0700

    refactor member transactions detail

---
+++ components/molecules/row-item/index.tsx
@@ -0,0 +1,14 @@
+interface RowItemProps {
+    label: string;
+    value: string | number;
+    className: string;
+}
+
+export default function RowItem(props: Partial<RowItemProps>) {
+  const { label, value, className } = props;
+  return (
+    <p className="text-lg color-palette-1 mb-20">
+      {label} <span className={`purchase-details ${className}`}>{value}</span>
+    </p>
+  );
+}
